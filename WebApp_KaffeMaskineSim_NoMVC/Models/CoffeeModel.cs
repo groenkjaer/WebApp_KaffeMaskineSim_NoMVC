84f4a3e34005310fcefe2d66a6153788d4d3fd14
Fillip <fill0246@student.eadania.dk>
2020-09-10 10:18:13 +0200

    Lots of things have happened i dont want to talk about it

---
+++ WebApp_KaffeMaskineSim_NoMVC/Models/CoffeeModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace WebApp_KaffeMaskineSim_NoMVC.Models
+{
+    public class CoffeeModel
+    {
+        public string CoffeeName { get; set; }
+        public Temperature Temperature { get; set; }
+        public Season Season { get; set; }
+        public bool Alchoholic { get; set; }
+        public List<string> Extra { get; set; }
+    }
+
+    public enum Temperature
+    {
+        Cold,
+        Medium,
+        Warm
+    }
+    public enum Season
+    {
+        Spring,
+        Summer,
+        Fall,
+        Winter
+    }
+}
---
+++ WebApp_KaffeMaskineSim_NoMVC/Pages/JSON/CoffeeMaker.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApp_KaffeMaskineSim_NoMVC.Models;
+
+namespace WebApp_KaffeMaskineSim_NoMVC.Pages.JSON
+{
+    public class CoffeeMakerModel : PageModel
+    {
+        [BindProperty]
+        public CoffeeModel Coffee { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
