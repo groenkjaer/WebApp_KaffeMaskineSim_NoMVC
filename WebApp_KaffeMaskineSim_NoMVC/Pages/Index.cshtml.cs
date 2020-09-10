e3c103e81ba4b2f7c863b81ec639710fbbf5238f
Fillip Jensen <FillipJensen@live.dk>
2020-09-10 09:26:59 +0200

    Add project files.

---
+++ WebApp_KaffeMaskineSim_NoMVC/Pages/Index.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace WebApp_KaffeMaskineSim_NoMVC.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost(string button)
+        {
+            Console.WriteLine("OnPost Hit");
+        }
+
+        public void OnPostKaffe()
+        {
+            Console.WriteLine("OnPostKaffe Hit");
+        }
+    }
+}
