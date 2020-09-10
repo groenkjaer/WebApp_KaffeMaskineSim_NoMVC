8187b2f7f8ed9c252c58cec836985bbb589903a2
Fillip <fill0246@student.eadania.dk>
2020-09-10 16:00:23 +0200

    TempDataExtension implemented

---
+++ WebApp_KaffeMaskineSim_NoMVC/Helpers/TempDataExtension.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace WebApp_KaffeMaskineSim_NoMVC.Helpers
+{
+    public static class TempDataExtension
+    {
+        public static void Set<T>(this ITempDataDictionary tempData, string key, T value) where T : class
+        {
+            tempData[key] = JsonSerializer.Serialize<T>(value);
+        }
+        public static T Get<T>(this ITempDataDictionary tempData, string key) where T : class
+        {
+            tempData.TryGetValue(key, out object o);
+            return o == null ? null : JsonSerializer.Deserialize<T>((string)o);
+        }
+    }
+}
