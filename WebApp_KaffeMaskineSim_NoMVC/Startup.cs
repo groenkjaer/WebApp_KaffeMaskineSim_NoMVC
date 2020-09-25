e178c700bca75f6bf06bba0236866a2de1297b16
Fillip <fill0246@student.eadania.dk>
2020-09-25 14:45:10 +0200

    Initial MVC setup ;)

--- WebApp_KaffeMaskineSim_NoMVC/Startup.cs
+++ WebApp_KaffeMaskineSim_NoMVC/Startup.cs
@@ -24,6 +24,7 @@ namespace WebApp_KaffeMaskineSim_NoMVC
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,7 +50,9 @@ namespace WebApp_KaffeMaskineSim_NoMVC
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
